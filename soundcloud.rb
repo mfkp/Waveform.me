4cf0b8975990e5231f69ade4f9ff0a2c85b1f6c9
Kyle Powers <kylepowers@gmail.com>
2012-01-24 13:01:59 -0800

    testing

--- soundcloud.rb
+++ soundcloud.rb
@@ -4,12 +4,16 @@ require 'uri'
 
 
 class SoundCloud < Sinatra::Base
+	get '/index' do
+		'hello world'
+	end
+
 	get '/get/:url' do
 	  res = Net::HTTP.get_response URI.parse(URI.unescape(params[:url]))
 	  content_type 'image/png'
 	  res.body
 	end
-	
+
 	# start the server if ruby file executed directly
 	run! if app_file == $0
 end
