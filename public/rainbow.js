01648d990bb65d84a224048acc75fc7992460a42
Kyle Powers <kylepowers@gmail.com>
2012-01-24 21:48:24 -0800

    updating the rainbow algorithm a lil

--- public/rainbow.js
+++ public/rainbow.js
@@ -4,12 +4,12 @@
 		var canvas = document.getElementById('rainbow');
 		var width = canvas.width;
 		var height = canvas.height;
-		var bgcolor = "rgba(255,255,255,1)";
+		var bgcolor = "rgba(255,255,255,0)";
 		var barWidth = 5; // pixels for progress indicator
 		var fadeSpeed = 6; // between 1 and 10, 1=fast, 10=slow
 		//var waveformType = 1; // 1 = mirrored, 2 = half waveform
 		var skeleton = true; // turn on skeleton mode #winning
-		var skeletonbgcolor = "rgba(255,255,255,.15)";
+		var skeletonbgcolor = "rgba(255,255,255,.05)";
 		var originalBarWidth = barWidth;
 
 		var ctx = canvas.getContext('2d');
@@ -20,10 +20,9 @@
 			var nextVolume = soundcloud.getNextVolume();
 			var waveformHeight = height * volume;
 			ctx.fillStyle = bgcolor;
-			ctx.fillRect(x,0,width-x,height); //background
+			ctx.clearRect(x,0,width-x,height);
 			ctx.fillStyle = "hsla(" + (color+x/fadeSpeed) % 360 + ", 100%, 50%, " + volume + ")";
-			ctx.fillRect(x-2,height/2 - (waveformHeight/2),1,waveformHeight); //middle bar
-			//ctx.fillStyle = "hsla(" + 360 - ((color+x/fadeSpeed) % 360) + ", 100%, 50%, " + _sc_volume + ")";
+			ctx.fillRect(x,height/2 - (waveformHeight/2),1,waveformHeight); //middle bar
 			if (barWidth > originalBarWidth) {
 				barWidth -= 1;
 			}
@@ -34,7 +33,6 @@
 			if (skeleton) {
 				ctx.fillStyle = skeletonbgcolor;
 				ctx.fillRect(x-3,height/2 - (waveformHeight/4),1,waveformHeight/2);
-				//ctx.fillRect(x-3,height/2 - (waveformHeight/16),1,waveformHeight/8);
 			}
 		}
 		scVisualizations.push(loop);
