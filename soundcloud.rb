fffb4876513fca932c1122fe8f06134bac8bccf4
Kyle Powers <kylepowers@gmail.com>
2012-01-24 21:48:35 -0800

    formatting some stuff

--- soundcloud.rb
+++ soundcloud.rb
@@ -2,17 +2,11 @@ require 'sinatra'
 require 'net/http'
 require 'uri'
 
-
-# class SoundCloud < Sinatra::Base
-	get '/get/*' do |url|
-	  if !url.match(/^http:\/\//)
-	  	url.sub!(/^http:\//, 'http://')
-	  end
-	  res = Net::HTTP.get_response URI.parse(URI.unescape(url))
-	  content_type 'image/png'
-	  res.body
-	end
-
-	# start the server if ruby file executed directly
-	# run! if app_file == $0
-# end
+get '/get/*' do |url|
+  if !url.match(/^http:\/\//)
+  	url.sub!(/^http:\//, 'http://')
+  end
+  res = Net::HTTP.get_response URI.parse(URI.unescape(url))
+  content_type 'image/png'
+  res.body
+end
