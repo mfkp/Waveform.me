f45fcc4afa9ef2a40a36745716e3359486e0a7e7
Kyle Powers <kylepowers@gmail.com>
2012-05-03 17:22:15 -0400

    guess we do still need that regex in there, stupid thing

--- soundcloud.rb
+++ soundcloud.rb
@@ -13,6 +13,9 @@ end
 
 #simple soundcloud image proxy so we can serve images through the same domain, with jsonp and base64 images
 get '/get/*' do |url|
+  if !url.match(/^http:\/\//)
+    url.sub!(/^http:\//, 'http://')
+  end
   image = open(url) {|f| f.read }
   return_image = Base64.encode64(image)
   type_prefix = "data:image/#{File.extname(url).gsub(/\./, '').sub(/jpg/, 'jpeg')};base64,"
